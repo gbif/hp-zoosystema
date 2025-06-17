db3298b4277653b39543838fe3998ad2073201a0
Chris (EJT) <chris.le-coquet-le-roux@mnhn.fr>
2025-06-17 17:11:00 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,179 +1,61 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({
-  baseTheme: 'light', extendWith: {
-      primary: themeStyle.colors.primary
-  }
-});
-
-var publisherKeys = [
-  '760d5f24-4c04-40da-9646-1b2c935da502',
-  '2e7df380-8356-4533-bcb3-5459e23c794e',
-  '8e1a97a0-3ca8-11d9-8439-b8a03c50a862',
-];
-
-var institutionKeys = [
-  '68fcaba0-61fe-4e78-a460-4f37654d025d',
-  '59f46093-8fae-47f3-a9ef-e5fd1d38e4fe',
-  '44e7a331-0270-4c39-ad64-91c0d8416480',
-];
+var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
+  primary: themeStyle.colors.primary,
+  fontSize: '16px'
+}});
+const publisherKey = 'e034534f-2cc7-4026-957a-dd034629514f';
 
 var siteConfig = {
-  "version": 3,
-  "pages": [
-      {
-          "id": "occurrenceSearch"
-      },
-      {
-          "id": "collectionSearch"
-      },
-      {
-          "id": "collectionKey"
-      },
-      {
-          "id": "datasetSearch"
-      },
-      {
-          "id": "datasetKey"
-      },
-      {
-          "id": "institutionKey"
-      },
-      {
-          "id": "literatureSearch"
-      }
-  ],
-  "disableInlineTableFilterButtons": false,
-  "availableCatalogues": [
-      "OCCURRENCE",
-      "DATASET",
-      "COLLECTION",
-      "LITERATURE"
-  ],
-  "dataHeader": {
-      "enableApiPopup": false,
-      "enableInfoPopup": false
+  routes: {
+    alwaysUseHrefs: false,
+    enabledRoutes: ['occurrenceSearch', 'datasetSearch', 'datasetKey', 'literatureSearch'],
   },
-  "theme": {
-      "primary": "#001972",
-      "borderRadius": 3,
-      "stickyOffset": "0px"
+  dataset: {
+    rootFilter: {
+      publishingOrg: publisherKey
+    },
+    excludedFilters: ['anyPublisherKey', 'datasetType', 'hostingOrganizationKey', 'networkKey', 'publishingCountryCode', 'license', 'projectId'],
+    highlightedFilters: ['q']
   },
-  "maps": {
-      "mapStyles": {
-          "defaultProjection": "MERCATOR",
-          "defaultMapStyle": "BRIGHT",
-          "options": {
-              "MERCATOR": [
-                  "BRIGHT",
-                  "NATURAL"
-              ]
+  occurrence: {
+    excludedFilters: ['publisherKey', 'publishingCountryCode'],
+    highlightedFilters: ['taxonKey', 'country', 'year', 'datasetKey'],
+    mapSettings: {
+      lat: 0,
+      lng: 0,
+      zoom: 1
+    },
+    // all the columns that are available to the user. This array defines the order they appear in. By default all all column are available.
+    availableTableColumns: ['features', 'country', 'stateProvince', 'locality', 'coordinates', 'year', 'eventDate', 'basisOfRecord', 'recordedBy', 'identifiedBy', 'recordNumber', 'catalogNumber', 'collectionCode', 'institutionCode', 'dataset'],
+    defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'catalogNumber', 'dataset'], // the columns showed by default. The order is not relevant, as it is defined in the list of available columns. The user can change what columns to show in the UI.
+    // You probably need help to configure the scope - so just ask
+    // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
+    // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
+    // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
+    rootPredicate: { type: 'equals', key: 'publishingOrg', value: publisherKey }, 
+    occurrenceSearchTabs: ['MAP', 'TABLE', 'DATASETS', 'CLUSTERS'] // what tabs should be shown
+    // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+  },
+  literature: {
+    rootFilter: {
+      predicate: {
+        type: 'and', predicates: [
+          {
+            type: 'in',
+            key: 'publishingOrganizationKey',
+            values: [publisherKey]
+          },
+          {
+            type: 'equals',
+            key: 'relevance',
+            value: 'GBIF_USED'
+          },
+          {
+            type: 'equals',
+            key: 'peerReview',
+            value: 'true'
           }
+        ]
       }
-  },
-  "languages": [
-      {
-          "code": "en",
-          "localeCode": "en",
-          "label": "English",
-          "default": true,
-          "textDirection": "ltr",
-          "iso3LetterCode": "eng",
-          "cmsLocale": "en-GB",
-          "gbifOrgLocalePrefix": "",
-          "mapTileLocale": "en"
-      },
-      {
-          "code": "da",
-          "localeCode": "da",
-          "label": "Dansk",
-          "default": false,
-          "textDirection": "ltr",
-          "iso3LetterCode": "dan",
-          "cmsLocale": "en-GB",
-          "gbifOrgLocalePrefix": "",
-          "mapTileLocale": "en"
-      }
-  ],
-  "messages": {},
-  "occurrenceSearch": {
-      "scope": {
-          "type": "in",
-          "key": "publishingOrg",
-          "values": [
-              "760d5f24-4c04-40da-9646-1b2c935da502",
-              "2e7df380-8356-4533-bcb3-5459e23c794e",
-              "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-          ]
-      },
-      "highlightedFilters": [
-          "taxonKey",
-          "verbatimScientificName",
-          "institutionKey",
-          "collectionKey",
-          "catalogNumber",
-          "recordedBy",
-          "identifiedBy"
-      ],
-      "excludedFilters": [
-          "occurrenceStatus",
-          "networkKey",
-          "hostingOrganizationKey",
-          "protocol",
-          "publishingCountry",
-          "institutionCode",
-          "collectionCode"
-      ],
-      "defaultEnabledTableColumns": [
-          "features",
-          "institutionKey",
-          "collectionKey",
-          "catalogNumber",
-          "country",
-          "year",
-          "recordedBy",
-          "identifiedBy"
-      ],
-      "tabs": [
-          "table",
-          "gallery",
-          "map",
-          "clusters",
-          "dashboard",
-          "download"
-      ],
-      "mapSettings": {
-          "lat": 52,
-          "lng": 12,
-          "zoom": 4.911544076366507
-      }
-  },
-  "collectionSearch": {
-      excludedFilters: ['country', 'active'],
-      // highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-      // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
-      scope: {
-          institutionKey: institutionKeys,
-          active: true
-      },
-  },
-  "institutionSearch": {},
-  "datasetSearch": {
-      excludedFilters: ['publishingCountry', 'networkKey', 'projectId', 'hostingOrg'],
-      highlightedFilters: ['q', 'type', 'publishingOrg', 'license'],
-      // defaultTableColumns: ['title', 'description', 'publisher', 'type', 'occurrenceCount', 'literatureCount'],
-      scope: {
-          publishingOrg: publisherKeys
-      },
-  },
-  "publisherSearch": {},
-  "literatureSearch": {
-      "scope": {
-          "type": "in",
-          "key": "publishingOrganizationKey",
-          "values": [
-              "760d5f24-4c04-40da-9646-1b2c935da502",
-              "2e7df380-8356-4533-bcb3-5459e23c794e",
-              "8e1a97a0-3ca8-11d9-8439-b8a03c50a862"
-          ]
-      }
+    }
   }
 }
